7d6f64d10e03c6546f7dbdc0493f6f626d18e004
guoyuyuan <guoyuyuan1010@163.com>
2025-12-23 20:27:45 +0800

    3R-Type 指令初步实现

--- TEMU/temu/src/cpu/3r-type.c
+++ TEMU/temu/src/cpu/3r-type.c
@@ -20,10 +20,38 @@ static void decode_3r_type(uint32_t instr) {
 	op_dest->reg = instr & 0x0000001F;
 }
 
+// temu/src/cpu/3r-type.c
 make_helper(or) {
+    decode_3r_type(instr);  
+    reg_w(op_dest->reg) = (op_src1->val | op_src2->val);
+    sprintf(assembly, "or\t%s,\t%s,\t%s", 
+            REG_NAME(op_dest->reg), REG_NAME(op_src1->reg), REG_NAME(op_src2->reg));
+}
 
-	decode_3r_type(instr);
-	reg_w(op_dest->reg) = (op_src1->val | op_src2->val);
-	sprintf(assembly, "or\t%s,\t%s,\t%s", REG_NAME(op_dest->reg), REG_NAME(op_src1->reg), REG_NAME(op_src2->reg));
+// temu/src/cpu/3r-type.c
+make_helper(add_w) {
+    decode_3r_type(instr);
+    uint32_t result = op_src1->val + op_src2->val;
+    reg_w(op_dest->reg) = result;
+    sprintf(assembly, "add.w\t%s,\t%s,\t%s", 
+            REG_NAME(op_dest->reg), REG_NAME(op_src1->reg), REG_NAME(op_src2->reg));
+}
+
+// temu/src/cpu/3r-type.c
+make_helper(xor) {
+    decode_3r_type(instr);
+    reg_w(op_dest->reg) = (op_src1->val ^ op_src2->val);
+    sprintf(assembly, "xor\t%s,\t%s,\t%s", 
+            REG_NAME(op_dest->reg), REG_NAME(op_src1->reg), REG_NAME(op_src2->reg));
+}
+
+// temu/src/cpu/3r-type.c
+make_helper(sll_w) {
+    decode_3r_type(instr);
+    // 只取低5位作为移位数量
+    uint8_t shift_amount = op_src2->val & 0x1F;
+    reg_w(op_dest->reg) = op_src1->val << shift_amount;
+    sprintf(assembly, "sll.w\t%s,\t%s,\t%s", 
+            REG_NAME(op_dest->reg), REG_NAME(op_src1->reg), REG_NAME(op_src2->reg));
 }
 
