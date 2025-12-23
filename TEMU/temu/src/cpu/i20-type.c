07e27b6d3cf998fa6d25b7e5ddaaf5d60ec7d9d8
guoyuyuan <guoyuyuan1010@163.com>
2025-12-23 20:43:35 +0800

    I20-Type 指令初步实现

--- TEMU/temu/src/cpu/i20-type.c
+++ TEMU/temu/src/cpu/i20-type.c
@@ -17,10 +17,29 @@ static void decode_i20_type(uint32_t instr) {
 	op_dest->reg = instr & 0x0000001F;
 }
 
+// temu/src/cpu/i20-type.c
 make_helper(lu12i_w) {
+    decode_i20_type(instr);  // 框架已提供
+    // 将20位立即数左移12位
+    reg_w(op_dest->reg) = (op_src2->val << 12);
+    sprintf(assembly, "lu12i.w\t%s,\t0x%04x", 
+            REG_NAME(op_dest->reg), op_src2->imm);
+}
 
-	decode_i20_type(instr);
-	reg_w(op_dest->reg) = (op_src2->val << 12);
-	sprintf(assembly, "lu12i.w\t%s,\t0x%04x", REG_NAME(op_dest->reg), op_src2->imm);
+// temu/src/cpu/i20-type.c
+make_helper(pcaddu12i) {
+    decode_i20_type(instr);
+    // 将20位立即数左移12位并符号扩展
+    int32_t imm = ((int32_t)op_src2->imm << 12) >> 12; // 符号扩展
+    imm <<= 12; // 左移12位
+    
+    // 当前PC (注意pc已映射为物理地址)
+    uint32_t current_pc = cpu.pc & 0x7FFFFFFF;
+    
+    // 计算结果 (保持32位结果)
+    reg_w(op_dest->reg) = (current_pc + imm) & 0xFFFFFFFF;
+    
+    sprintf(assembly, "pcaddu12i\t%s,\t0x%05x", 
+            REG_NAME(op_dest->reg), op_src2->imm);
 }
 
